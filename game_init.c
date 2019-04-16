8d3fe323bedcf8a0e85fecb3002a7f746236c5f7
daianamorjolic <49615616+daianamorjolic@users.noreply.github.com>
2019-04-16 19:34:16 +0100

    Add files via upload

---
+++ game_init.c
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+#include "game_init.h"
+#include <stdio.h>
+
+
+/*
+ * This function creates the board for the first time
+ *
+ * Input: board - a 6x9 array of squares
+ *
+ */
+void initialize_board(Square board[NUM_ROWS][NUM_COLUMNS]){
+
+    for (int i =0; i< NUM_ROWS; i++){
+        for(int j =0; j < NUM_COLUMNS; j++){
+            //creates an obstacle square at positions (0,3), (1,6), (2,4), (3,5), (4,2) and (5,7)
+            if((i == 0 && j==3) || (i == 1 && j == 6) || (i ==2 && j ==4)
+                    || (i == 3 && j ==5) || (i==4 && j==2) || (i==5 && j==7)){
+                board[i][j].type = OBSTACLE;
+            } else{
+                //creates a normal square otherwise
+                board[i][j].type = NORMAL;
+            }
+            board[i][j].stack = NULL;
+        }
+    }
+}
+
+
+ /*
+ * This function creates players for the first time
+ *
+ * Input: the array of players to be initialized
+ * Output: The number of players of the game
+ */
+int initialize_players(Player players[]){
+
+    int numPlayer = 0; //initialising number of players to zero at beginning of the game
+
+    //asking how many players will play and storing value in variable player_num
+    printf("Please enter the number of players (between 2 and 6): ");
+    scanf("%d", &numPlayer);
+
+    //checking if the number of players is valid
+    if(numPlayer < 2 || numPlayer > 6)
+    {
+        //error message when player number is too small or too high
+        printf("Sorry, the game can accommodate only between 2 and 6 players.\n"
+               "Please start again.\n");
+        return -1;
+    }
+
+    else
+    {
+        //going through the number of players to fill in their names and color choices
+        for (int pl = 0; pl < numPlayer; ++i)
+        {
+            printf("\nPlease enter name of player %d: ", pl+1);
+            scanf("%s", &players[pl].player_name); //scanning name of player
+
+            //asking user to pick color they wish to use
+            printf("Please pick the color you wish to play with:\n"
+                   "Input 1 for Red.\n"
+                   "Input 2 for Blue.\n"
+                   "Input 3 for Green\n"
+                   "Input 4 for Yellow.\n"
+                   "Input 5 for Pink.\n"
+                   "Input 6 for Orange.\n");
+
+            //scanning choice into a variable
+           int color_choice;
+           scanf("%d", &color_choice);
+           color_choice--;  //decrementing choice as ENUMS are ordered from zero
+           players[pl].col = color_choice;    //placing color choice in corresponding member of struct of that player
+
+            //ASK IF THIS IS CORRECT FOR IMPLEMENTATION
+
+            //checking if color is already taken
+
+           /*if (players[pl-1].col == players[pl].col)
+           {
+               //error message to screen if color is already taken
+               printf("Each player must have a different color!\n"
+                      "Please pick a different color.\n
+                       "Input 1 for Red.\n"
+                       "Input 2 for Blue.\n"
+                       "Input 3 for Green\n"
+                       "Input 4 for Yellow.\n"
+                       "Input 5 for Pink.\n"
+                       "Input 6 for Orange.\n");
+                //scanning new color choice
+                scanf("%d", &color_choice);
+                players[pl].col = color_choice;
+           }
+        }
+*/
+        return numPlayer;
+
+}
+
+
+
+
---
+++ game_logic.c
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+#include "game_init.h"
+#include <stdio.h>
+
+void printLine();
+
+/*
+ * Returns the first letter associated with the color of the token
+ *
+ * Input: t - pointer to a token
+ * Output: initial of the color of the token
+ */
+char print_token(Token *t){
+    if((*t).col== PINK) return 'P';
+    if((*t).col== RED) return 'R';
+    if((*t).col== BLU) return 'B';
+    if((*t).col== GREEN) return 'G';
+    if((*t).col== ORANGE) return 'O';
+    if((*t).col== YELLOW) return 'Y';
+    return '\0';
+}
+
+/*
+ * Prints the board
+ *
+ * Input: the board to be printed.
+ */
+void print_board(Square board[NUM_ROWS][NUM_COLUMNS]){
+    printf("                THE BOARD\n");
+    for(int i =0; i < NUM_ROWS; i++){
+
+        //prints an horizontal line
+        printLine();
+        //prints the row number
+        printf(" %d ", i);
+        char c = '\0' ;
+        //if the square (i,j) is occupied,
+        //c is assigned the initial of the color of the token that occupies the square
+        for (int j = 0; j < NUM_COLUMNS; j++){
+            if(board[i][j].stack != NULL){
+                c = print_token(board[i][j].stack);
+            }
+            //if the square (i,j) is empty
+            else{
+                //c is assigned 'X' if the square represents an obstacle
+                if(board[i][j].type == OBSTACLE)
+                    c = 'X';
+                //c is assigned an empty space otherwise
+                else c = ' ';
+            }
+            printf("| %c ", c);
+        }
+        printf ("|\n");
+    }
+    printLine();
+    //prints the number of the columns at the end of the board
+    printf("     0   1   2   3   4   5   6   7   8\n");
+}
+
+void printLine(){
+  printf("   -------------------------------------\n");
+}
+
+/*
+ * Place tokens in the first column of the board
+ *
+ * Input: board - a 6x9 array of squares that represents the board
+ *        players - the array of the players
+ *        numPlayers - the number of players
+ */
+void place_tokens(Square board[NUM_ROWS][NUM_COLUMNS], Player players[], int numPlayers){
+
+    int tok = 0;    //initialising token number to zero
+    while (tok !=4)
+    {
+        int pl = 0; //initialising player number to zero
+        while ( pl != numPlayers)
+        {
+            int row_num;
+            printf("Player %d: place token %d on a row of your choice from 1 to 6: ", pl+1, tok+1);
+            scanf("%d", &row_num);  //scan row number from user
+            row_num -=1;  //decreasing row value by 1 as two dimensional matrix starts at 0
+
+            Token *new_tok = malloc(sizeof(Token));
+            new_tok.color = players[pl].col; //adding the colour of the player to the token
+            new_tok.belowPtr = board[row_num][0].top_stack;
+            board[row_num][0].top_stack = newPtr;
+
+            pl++;   //incrementing player number
+        }
+
+        tok++;  //incrementing token number
+    }
+
+}
+
+
+// returns the name of the color from an enum Color type
+char *color_name(enum color color) {
+    switch(color) {
+        case 0: return "red";
+        case 1: return "blue";
+        case 2: return "green";
+        case 3: return "yellow";
+        case 4: return "pink";
+        case 5: return "orange";
+        default: return "color blind";
+    }
+}
+
+// helper function to move any token from one location to another
+void move_token(Square board[][NUM_COLUMNS], int cur_row, int cur_col, int new_row, int new_col) {
+    Token *token = board[cur_row][cur_col].top_stack; // token we want to relocate
+    board[cur_row][cur_col].top_stack = token->belowPtr; // remove the token
+
+    // below is what's now the top token at the new location
+    token->belowPtr = board[new_row][new_col].top_stack;
+    board[new_row][new_col].top_stack = token; // place token on top of the new location
+}
+
+/*
+ * Place tokens in the first column of the board
+ *
+ * Input: board - a 6x9 array of squares that represents the board
+ *        players - the array of the players
+ *        numPlayers - the number of players
+ */
+void play_game(Square board[NUM_ROWS][NUM_COLUMNS], Player players[], int numPlayers) {
+    // for each player
+    for (int i = 0; i < numPlayers; ++i) {
+        printf("\nPlayer's turn: %s(%s)", players[i].player_name, color_name(players[i].col));
+        // take his turn:
+        // roll die and tell the player his die roll
+        int die = rand() % 6;
+        puts("You rolled: %d": die);
+
+        // ask the player if he wants to move sideways
+        int answer;
+        puts("Do you want to move sideways? (1 for yes, 0 for no)");
+        scanf("%d", &answer);
+
+        // if he says yes
+        if (answer == 1) {
+            // ask which token he wants to move (in coordinates) (it must be his token)
+            // and in which direction (up or down)
+            // move the token
+        }
+
+        // once you're done with the sideways move
+        // ask the player which token does he wants to move according the die result row (it doesn't have to be his token, but it has to be the top one)
+        // move the token forward (one column ahead) ONLY IF IT IS NOT ON AN OBSTACLE SQUARE
+
+        // check if he won and break
+
+        // check if obstacles are still pbstacles
+
+        // once the last player took his turn
+        if (i == numPlayer - 1) {
+            i = -1; // reset to the first player
+        }
+    }
+
+}
+
+
+
